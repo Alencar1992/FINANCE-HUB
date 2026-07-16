a3b6f613c1d6e30ea9e05d27d8b0cf7caf7b5f3e
LUCAS ALENCAR <lucas.aluncar.l70@gmail.com>
2026-07-16 16:52:09 -0300

    sync dashboard notifications calendar and installments

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='finance-hub-v1';
+const CACHE='finance-hub-v2';
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/FINANCE-HUB/','/FINANCE-HUB/manifest.webmanifest','/FINANCE-HUB/icon.svg']))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x))))));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('/FINANCE-HUB/'))))});
